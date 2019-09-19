caa1144778ec67e8a082d8722035e582efb8d24c
DoorMamu <mmkkolhapur@gmail.com>
2019-09-19 15:03:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/expandedlist/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.expandedlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+MyExpandableListAdapter expListAdapter;
+ExpandableListView expListView;
+List<String> listDataHeader;
+HashMap<String,List<String>> listDataChild;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        expListView =findViewById(R.id.exp_lst);
+        //prepare data for list
+        prepareListData();
+        expListAdapter= new MyExpandableListAdapter(this,listDataHeader,listDataChild);
+        expListView.setAdapter(expListAdapter);
+
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int listPos) {
+                Toast.makeText(MainActivity.this, listDataHeader.get(listPos)+" List Expanded", Toast.LENGTH_SHORT).show();
+            }
+        });
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View view, int listPos, int childPos, long id) {
+                Toast.makeText(MainActivity.this,
+                        listDataHeader.get(listPos)+"->"+listDataChild.get(listDataHeader.get(listPos)).get(childPos),
+                        Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+        });
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int listPos) {
+                Toast.makeText(MainActivity.this,  listDataHeader.get(listPos)+" List Collapsed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+//*********************step1******************************
+    private void prepareListData()
+    {
+        listDataHeader=new ArrayList<>();
+        listDataChild=new HashMap<>();
+        //Add header data
+        listDataHeader.add("BIMSTEC");
+        listDataHeader.add("BRICS");
+        listDataHeader.add("G7");
+        listDataHeader.add("G20");
+
+        //Adding child data
+
+        List<String> top250=new ArrayList<>();
+        top250.add("India");
+        top250.add("Indonesia");
+        top250.add("Mayanmar");
+        top250.add("SriLanka");
+        top250.add("Thailand");
+        top250.add("Nepal");
+        top250.add("Bhutan");
+
+        List<String> top280=new ArrayList<>();
+        top280.add("Brazil");
+        top280.add("Russia");
+        top280.add("India");
+        top280.add("China");
+        top280.add("South Africa");
+
+        List<String> top290=new ArrayList<>();
+        top290.add("France");
+        top290.add("USA");
+        top290.add("Japan");
+        top290.add("Canada");
+        top290.add("Italy");
+        top290.add("UK");
+        top290.add("Germany");
+
+        List<String> top300=new ArrayList<>();
+        top300.add("France");
+        top300.add("USA");
+        top300.add("Japan");
+        top300.add("Canada");
+        top300.add("Italy");
+        top300.add("UK");
+        top300.add("Germany");
+        top300.add("Argentina");
+        top300.add("Australia");
+        top300.add("Brazil");
+        top300.add("China");
+        top300.add("Russia");
+        top300.add("Mexico");
+        top300.add("Saudi Arabia");
+        top300.add("South Africa");
+        top300.add("South Korea");
+        top300.add("Turkey");
+        top300.add("European Union");
+
+
+
+
+        List<String> top=new ArrayList<>();
+        listDataChild.put(listDataHeader.get(0),top250);
+        listDataChild.put(listDataHeader.get(1),top280);
+        listDataChild.put(listDataHeader.get(2),top290);
+        listDataChild.put(listDataHeader.get(3),top300);
+
+    }
+}
---
+++ app/src/main/java/com/example/expandedlist/MyExpandableListAdapter.java
@@ -0,0 +1,95 @@
+package com.example.expandedlist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+class MyExpandableListAdapter extends BaseExpandableListAdapter {
+    //************************Step 2***************************
+    Context context;
+    List<String> expListHeader;
+    HashMap<String, List<String>> listDataChild;
+    public MyExpandableListAdapter(){}
+    public MyExpandableListAdapter(Context context, List<String> expListHeader, HashMap<String, List<String>> listDataChild)
+    {
+        this.context=context;
+        this.expListHeader=expListHeader;
+        this.listDataChild=listDataChild;
+    }
+    //Get number of headers
+    @Override
+    public int getGroupCount() {
+        return this.expListHeader.size();
+    }
+    //return size of the current selected header's childs
+    @Override
+    public int getChildrenCount(int listPos) {
+        return this.listDataChild.get(this.expListHeader.get(listPos)).size();
+    }
+
+    //return the Header Strings
+    @Override
+    public Object getGroup(int listPos) {
+        return this.expListHeader.get(listPos);
+    }
+    //Retun the items in the header
+    @Override
+    public Object getChild(int listPos, int expListPos) {
+        return this.listDataChild.get(this.expListHeader.get(listPos)).get(expListPos);
+    }
+
+    @Override
+    public long getGroupId(int listPos) {
+        return listPos;
+    }
+
+    //return the position of the child
+    @Override
+    public long getChildId(int listPos, int expandedListPos) {
+        return expandedListPos;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int listPos, boolean isExpanded, View convertView, ViewGroup parent) {
+        String listTitle =(String)getGroup(listPos);
+        if(convertView==null)
+        {
+            LayoutInflater layoutInflater=(LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView=layoutInflater.inflate(R.layout.list_group,null);
+        }
+        TextView expandTextView=convertView.findViewById(R.id.expanded_list_group);
+        expandTextView.setText(listTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int listPos, int expandedListPos, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText =(String)getChild(listPos,expandedListPos);
+        if(convertView==null)
+        {
+            LayoutInflater layoutInflater=(LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView=layoutInflater.inflate(R.layout.list_item,null);
+        }
+        TextView expandTextView=convertView.findViewById(R.id.expanded_list_item);
+        expandTextView.setText(expandedListText);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+}
